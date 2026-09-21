6fe0c5a92f05580f5bb3ad4e800fac76e2d24fc0
Alex <hp.val84@gmail.com>
2026-09-21 14:50:42 -0500

    dark feature, color feature and button added

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 // query selectors
 
 const squareGrid = document.querySelector(".square--grid");
+const button = document.querySelector("button");
 
 // boolean values to check if the border radious of the small squares in the upper-corners have been created yet
 
@@ -11,6 +12,14 @@ let upperRightCorner = false;
 
 const createGrid = (grids) => {
   const length = grids;
+
+  // check if grids have been created in order to remove them
+  if (document.querySelector(".row--container")) {
+    const allElements = document.querySelectorAll(".row--container");
+    for (let i = 0; i < allElements.length; i++) {
+      squareGrid.removeChild(allElements[i]);
+    }
+  }
   for (let i = 0; i < length; i++) {
     // Create new row
     const newRow = document.createElement("div");
@@ -26,12 +35,54 @@ const createGrid = (grids) => {
   }
 };
 
-createGrid(7);
-
+createGrid(16);
 // create function that randomize the colors of the small squares
 
-// add event listener to small squares (via parent element) and use randomize colors to color the small squares
+const createRandomColor = () => {
+  return Math.round(Math.random() * 255);
+};
 
+const addDarkness = (target) => {
+  const colors = target.style.backgroundColor
+    .slice(4)
+    .replace(")", "")
+    .split(", ");
+
+  const r = Number(colors[0]);
+  const g = Number(colors[1]);
+  const b = Number(colors[2]);
+
+  target.style.backgroundColor = `rgb(${Math.floor(r * 0.9)}, ${Math.floor(g * 0.9)}, ${Math.floor(b * 0.9)})`;
+};
+
+const addRGBOrDarkness = (event) => {
+  const target = event.target;
+  if (target.classList.contains("square--grid")) {
+    return;
+  }
+
+  const red = createRandomColor();
+  const green = createRandomColor();
+  const blue = createRandomColor();
+
+  if (target.classList.contains("colored")) {
+    addDarkness(target);
+    return;
+  }
+
+  target.style.backgroundColor = `rgb(${red}, ${green}, ${blue})`;
+  target.classList.add("colored");
+};
+
+const buttonClick = () => {
+  const number = Number(prompt("select your number"));
+  createGrid(number);
+};
+
+squareGrid.addEventListener("mouseover", addRGBOrDarkness);
+
+// add event listener to small squares (via parent element) and use randomize colors to color the small squares
+button.addEventListener("click", buttonClick);
 // add event listener to button create new grid
 
 // -----------------additional---------------
